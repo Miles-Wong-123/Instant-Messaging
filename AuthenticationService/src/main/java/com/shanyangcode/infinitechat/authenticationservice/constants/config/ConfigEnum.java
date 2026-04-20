fadb5188cc3c9aaf263e41b93e72a22feb2b4f2a
Miles <wanghirili@gmail.com>
2026-04-20 19:38:16 +0800

    Initial commit

---
+++ AuthenticationService/src/main/java/com/shanyangcode/infinitechat/authenticationservice/constants/config/ConfigEnum.java
@@ -0,0 +1,22 @@
+package com.shanyangcode.infinitechat.authenticationservice.constants.config;
+
+import lombok.Getter;
+
+@Getter
+public enum ConfigEnum {
+    SMS_ACCESS_KEY_ID("smsAccessKeyId", "原来的AccessKeyId"),
+    SMS_ACCESS_KEY_SECRET("smsAccessKeySecret","原来的AccessKeySecret"),
+
+    SMS_SIG_NAME("smsSigName","无夕教育科技"),
+    SMS_TEMPLATE_CODE("smsTemplateCode","SMS_471490089"),
+    TOKEN_SECRET_KEY("tokenSecretKey","goat");
+
+
+    private final String value;
+    private final String text;
+
+    ConfigEnum(String text, String value){
+        this.text = text;
+        this.value = value;
+    }
+}
